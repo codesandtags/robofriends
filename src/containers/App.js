e75e4762384102700b9bd4d7f36bf2b8bb37d4f3
Edwin Torres <codesandtags@gmail.com>
2020-05-07 21:50:09 -0500

    feature: updated logos and manifest

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,7 +19,7 @@ function App() {
 
     return (
         <div className="tc">
-            <GithubCorner url="https://github.com/codesandtags"/>
+            <GithubCorner url="https://github.com/codesandtags/robofriends"/>
             <h1 className="ma4 mt5-m f2 f1-l">RoboFriends</h1>
             <SearchBox searchChange={onSearchRobot} />
             <CardList robots={filteredRobots}/>
