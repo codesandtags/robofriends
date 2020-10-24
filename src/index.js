cc224da3737b77e7e22c44c772ed1afa73050138
Edwin Torres <codesandtags@gmail.com>
2020-10-24 16:57:57 -0500

    feature: Updated versions and added prettier

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import { createLogger } from 'redux-logger/src';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunkMiddleware from 'redux-thunk';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { createLogger } from 'redux-logger/src'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunkMiddleware from 'redux-thunk'
 
-import './index.css';
-import App from './containers/App';
-import 'tachyons';
+import './index.css'
+import App from './containers/App'
+import 'tachyons'
 
-import * as serviceWorker from './serviceWorker';
-import { requestRobots, searchRobots } from './store/reducers';
+import * as serviceWorker from './serviceWorker'
+import { requestRobots, searchRobots } from './store/reducers'
 
-const logger = createLogger();
+const logger = createLogger()
 const store = createStore(
     combineReducers({ searchRobots, requestRobots }),
     composeWithDevTools(applyMiddleware(thunkMiddleware, logger))
-);
+)
 
 ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
-            <App/>
+            <App />
         </React.StrictMode>
     </Provider>,
     document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
